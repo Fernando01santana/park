4a398b8b6867fc9ff7513fad609695afdf486fb4
Fernando01santana <cool.fernando23@gmail.com>
2023-01-24 23:36:55 -0300

    feat: insert middleware in module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,14 +1,26 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as bodyParser from 'body-parser';
+import { DocumentValidateMiddleware } from 'src/shared/middlewares/userMiddleware';
 import { AppController } from './controller/users.controller';
 import { Users } from './entites/user.entity';
 import UserRepository from './repositorie/userRepository';
 import { UserService } from './services/users.service';
-
 @Module({
   imports: [TypeOrmModule.forFeature([Users])],
   controllers: [AppController],
-  providers: [UserService,UserRepository],
-  exports:[]
+  providers: [UserService, UserRepository, DocumentValidateMiddleware],
+  exports: [],
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(bodyParser.json(), DocumentValidateMiddleware)
+      .forRoutes({ path: 'user/create', method: RequestMethod.POST });
+  }
+}
